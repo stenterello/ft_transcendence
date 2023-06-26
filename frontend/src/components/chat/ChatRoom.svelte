57d691cabcc2c1f0ce8254dfa42f3f342078ca6b
m0nt4lb4n0 <davide.delladio@gmail.com>
2023-06-26 19:30:25 +0200

    room management structure

--- frontend/src/components/chat/ChatRoom.svelte
+++ frontend/src/components/chat/ChatRoom.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
-    import { statusChange } from "../../data";
+    import { statusChange, socket, roomSelected, userInfo } from "../../data";
     import OnlineUsers from "./OnlineUsers.svelte";
+    import RoomManagement from "./RoomManagement.svelte";
 
     export let  chat: string | null = null;
+    let         roomManagement: boolean = false;
+    let         optionChange: boolean = false;
 
     async function  getMessages(): Promise<Object> {
         const   res: Response = await fetch('http://localhost:3000/chat/' + chat);
@@ -20,6 +23,52 @@
         }
     }
 
+    function    leaveRoom(): void {
+        $socket.emit('leaveRoom', JSON.stringify({'room': $roomSelected}))
+        $roomSelected = undefined;
+    }
+
+    function    demoteAdmin(user: string): void {
+        $socket.emit('demoteAdmin', JSON.stringify({'user': user, 'room': $roomSelected}));
+        optionChange = (optionChange) ? false : true;
+    }
+
+    function    promoteAdmin(user: string): void {
+        $socket.emit('promoteAdmin', JSON.stringify({'user': user, 'room': $roomSelected}));
+        optionChange = (optionChange) ? false : true;
+    }
+
+    function    kickUser(user: string): void {
+        $socket.emit('kickUser', JSON.stringify({'user': user, 'room': $roomSelected}));
+        optionChange = (optionChange) ? false : true;
+    }
+
+    function    banUser(user: string): void {
+        $socket.emit('banUser', JSON.stringify({'user': user, 'room': $roomSelected}));
+        optionChange = (optionChange) ? false : true;
+    }
+
+    function    unbanUser(user: string): void {
+        $socket.emit('banUser', JSON.stringify({'user': user, 'room': $roomSelected}));
+        optionChange = (optionChange) ? false : true;
+    }
+
+    function    muteUser(user: string): void {
+        $socket.emit('mute', JSON.stringify({'user': user, 'room': $roomSelected}));
+        optionChange = (optionChange) ? false : true;
+    }
+
+    function    unmuteUser(user: string): void {
+        $socket.emit('unmute', JSON.stringify({'user': user, 'room': $roomSelected}));
+        optionChange = (optionChange) ? false : true;
+    }
+
+    function    changePassword(): void {
+        let newPassword: string = prompt('What\'s the new password?');
+        $socket.emit('changePwd', JSON.stringify({'room': $roomSelected, 'password': newPassword}));
+        optionChange = (optionChange) ? false : true;
+    }
+
 </script>
 
 {#await getRoomObject()}
@@ -34,13 +83,66 @@
                 <p>loading</p>
             {:then messages}
                 <div id="messages">
-                    <h4>{roomInfo['name']}</h4>
-                    {#if Object.values(messages).length > 0}
-                        <ul>
+                    <RoomManagement {roomInfo} on:options={ () => {roomManagement = (roomManagement) ? false : true;} } />
+                    {#if roomManagement === false}
+                        {#if Object.values(messages).length > 0}
+                            <ul>
 
-                        </ul>
+                            </ul>
+                        {:else}
+                            <p>still no messages</p>
+                        {/if}
                     {:else}
-                        <p>still no messages</p>
+                        {#key optionChange}
+                            {#if roomInfo['admins'].includes($userInfo['username']) === false}
+                                <button on:click|preventDefault={leaveRoom}>Leave room</button>
+                            {:else}
+                                {#if roomInfo['password'] !== null}
+                                    <button on:click|preventDefault={changePassword}>Change password</button>
+                                {/if}
+                                <button on:click|preventDefault={leaveRoom}>Delete room</button>
+                                <h4>Members management</h4>
+                                <ul class="member-options">
+                                    {#each roomInfo['members'] as member}
+                                        {#if member !== $userInfo['username']}
+                                            {#if roomInfo['admins'].includes(member)}
+                                                <li>
+                                                    <p>{member}</p>
+                                                    <div>
+                                                        <button on:click|preventDefault={() => demoteAdmin(member) }>Demote admin</button>
+                                                    </div>
+                                                </li>
+                                            {:else}
+                                                <li>
+                                                    <p>{member}</p>
+                                                    <div>
+                                                        <button on:click|preventDefault={() => kickUser(member) }>Kick user</button>
+                                                        <button on:click|preventDefault={() => banUser(member) }>Ban user</button>
+                                                        {#if roomInfo['mutelist'].includes(member) === false}
+                                                            <button on:click|preventDefault={() => muteUser(member) }>Mute user</button>
+                                                            <button on:click|preventDefault={() => promoteAdmin(member) }>Promote to admin</button>
+                                                        {:else}
+                                                            <button on:click|preventDefault={() => unmuteUser(member) }>Unmute user</button>
+                                                        {/if}
+                                                    </div>
+                                                </li>
+                                            {/if}
+                                        {/if}
+                                    {/each}
+                                </ul>
+                                <h4>Banned users</h4>
+                                <ul class="member-options">
+                                    {#each roomInfo['banlist'] as banned}
+                                    <li>
+                                        <p>{banned}</p>
+                                        <div>
+                                            <button on:click|preventDefault={() => unbanUser(banned) }>Unban user</button>
+                                        </div>
+                                    </li>
+                                    {/each}
+                                </ul>
+                            {/if}
+                        {/key}
                     {/if}
                 </div>
             {/await}
@@ -63,17 +165,13 @@
         border: 1px solid black;
         height: 75%;
     }
-    h4 {
-        margin: 0;
-        padding: 0;
-        font-family: 'TrashHand';
-        color: #fcd612;
-        background-color: black;
-        height: 10%;
-    }
     ul {
         margin: 0;
         height: 90%;
         padding: 0;
+    }
+    .member-options > li {
+        display: flex;
+        justify-content: space-between;
     }
 </style>
---
+++ frontend/src/components/chat/RoomManagement.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { createEventDispatcher } from "svelte";
+
+	export let	roomInfo: Object;
+	const		dispatch = createEventDispatcher();
+
+	function	toggleManagement(): void {
+		dispatch('options', null);
+	}
+</script>
+
+<div id="chat-header">
+	<h4>{roomInfo['name']}</h4>
+	<div class="settings">
+		<img src="wrench.png" alt="settings" />
+		<button on:click={toggleManagement} ></button>
+	</div>
+</div>
+
+<style>
+	#chat-header {
+		display: flex;
+		background-color: black;
+		justify-content: space-between;
+	}
+	h4 {
+        margin: 0;
+        padding: 0;
+        font-family: 'TrashHand';
+        color: #fcd612;
+		font-size: 2em;
+    }
+	.settings {
+		position: relative;
+		padding: 3px;
+		width: 7%;
+		background-color: white;
+	}
+	.settings > img {
+		width: 100%;
+		height: 100%;
+	}
+	.settings > button {
+		position: absolute;
+		top: 0;
+		left: 0;
+		height: 100%;
+		width: 100%;
+		background-color: rgba(255,255,255,0);
+	}
+</style>
